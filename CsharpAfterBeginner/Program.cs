ddfb51a73634b8e217f7abb908986c2f0ffcac16
Ergin Çetin <59891652+ergincetn01@users.noreply.github.com>
2021-10-26 23:37:18 +0300

    Add files via upload

---
+++ CsharpAfterBeginner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAfterBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            a += 45;
+            Console.WriteLine("new a value: {0}", a);
+            
+
+            double f= 456.9;
+            Console.WriteLine("type conv...\n");
+            Console.WriteLine("converted float: {0}", (int)f);
+Console.Read();
+
+
+
+
+
+        }
+    }
+}
