6aecc9f2ae65c2f246a39ccdc5b5f77624ac447f
Ergin Çetin <59891652+ergincetn01@users.noreply.github.com>
2021-10-26 23:45:02 +0300

    Add files via upload

---
+++ ObjectDataType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object a = "gffdhhsdh";
+            object f = 456163;
+            Console.WriteLine("{0} - {1}", 
+            a, f);
+
+            Console.ReadLine();
+
+        }
+    }
+}
