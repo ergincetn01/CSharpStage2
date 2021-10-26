2d3db163b4ac39298221721f37a8f1248b9d075b
Ergin Çetin <59891652+ergincetn01@users.noreply.github.com>
2021-10-26 23:49:27 +0300

    Add files via upload

---
+++ Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int l = 45;
+            int f = l + 74;
+            double d = 12.6;
+            int dd = (int)d;
+
+
+            int h = 4 + dd;
+            Console.WriteLine(f / 5);
+            Console.WriteLine(h);
+            Console.WriteLine(dd);
+            Console.WriteLine(d);
+
+            object m = f % 5;
+            Console.WriteLine("f modulus 5: {0}", m);
+            Console.ReadLine();
+
+        }
+    }
+}
