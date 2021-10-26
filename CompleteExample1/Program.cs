5a069be597697459d6324a85e82113516ea6b0c3
Ergin Çetin <59891652+ergincetn01@users.noreply.github.com>
2021-10-26 23:44:39 +0300

    Add files via upload

---
+++ CompleteExample1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteExample1
+{
+    class DrivingLisence {
+        public int age;
+
+        public int Check(int a)
+        {
+            this.age = a;
+            if (a < 0)
+            {
+                Console.WriteLine("negative numbers cannot be an age value!!!!");
+                Check(a);
+            }
+            else if (age >= 18)
+            {
+                return 1;
+
+            }
+            return 0;
+   
+        }
+        public string checkRes()
+        {
+            int res = Check(age);
+
+            if (res == 0)
+            {
+                Console.WriteLine("not available for driving lisence");
+
+            }
+            else if (res==1)
+            {
+                Console.WriteLine("available");
+            }
+
+            return string.Format("---");
+        }
+                
+    }
+
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            Console.WriteLine("type your age: ");
+            age = Int32.Parse(Console.ReadLine());
+            DrivingLisence obj = new DrivingLisence();
+            object obj1 = obj.Check(age);
+            object ress = obj.checkRes();
+
+            Console.Read();
+
+         
+
+        }
+    }
+}
